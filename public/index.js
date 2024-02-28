4d3f3d338338d7303d50775e209c1b54be046d43
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-02-28 01:18:53 -0800

    initializing files

---
+++ public/index.js
@@ -0,0 +1,15 @@
+/**
+ * Sunny Tian
+ * 2/28/24
+ *
+ * This is my script for the english 208 website.
+ */
+
+"use strict";
+(function() {
+  window.addEventListener("load", init);
+
+  function init() {
+    // do some stuff
+  }
+})
