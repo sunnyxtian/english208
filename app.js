00e1644dd1f0aec2639c773704e2f3f6e84f5f51
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-03-16 13:27:44 -0700

    setup app.js packages

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+/**
+ * Sunny Tian
+ * 3/16/24
+ */
+
+// setup
 "use strict";
 
 const express = require('express');
 const app = express();
 
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const multer = require("multer");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(multer().none());
+app.use(express.text());
+
+
+// global module variables
+const SERVER_ERROR = 500;
+const PARAM_ERROR = 400;
+const HOST_NUM = 8080;
+
 app.get("/posts", function(req, res) {
   res.type("text").send("hello world");
 });
-app.listen(8080);
+
+app.use(express.static("public"));
+const PORT = process.env.PORT || HOST_NUM;
+app.listen(PORT);
