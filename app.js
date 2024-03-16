60b66d79b064e894deb4831cf00385dee93dcf19
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-03-16 16:18:43 -0700

    Added the scrobbles database and successfully gets scrobbles from endpoint

--- app.js
+++ app.js
@@ -22,12 +22,49 @@ app.use(express.text());
 
 // global module variables
 const SERVER_ERROR = 500;
+const SERVER_ERR_MSG = "Server error";
 const PARAM_ERROR = 400;
 const HOST_NUM = 8080;
 
-app.get("/posts", function(req, res) {
-  res.type("text").send("hello world");
+app.get("/getScrobbles", async (req, res) => {
+  let month = req.query.month;
+  let year = req.query.year;
+
+  try {
+    let result = await getScrobblesByMonth(month, year);
+    res.json(result);
+  } catch (error) {
+    res.status(SERVER_ERROR).type("text")
+      .send(SERVER_ERR_MSG);
+  }
 });
+
+async function getScrobblesByMonth(month, year) {
+  let searchTerm = month + " " + year;
+  try {
+    let db = await getDbConnection();
+    let query = "SELECT artist, album, song, date ";
+    query += "FROM scrobbles ";
+    query += "WHERE date LIKE '%";
+    query += searchTerm;
+    query += "%'";
+
+    let scrobbles = await db.all(query);
+    await db.close();
+    return scrobbles;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+async function getDbConnection() {
+  const db = await sqlite.open({
+    filename: "lastfm.db",
+    driver: sqlite3.Database
+  });
+  return db;
+}
 
 app.use(express.static("public"));
 const PORT = process.env.PORT || HOST_NUM;
